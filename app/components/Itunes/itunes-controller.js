24e54787b1d9dababc171dfe385d442492b104e2
JordanBriscoe <briscoe41@gmail.com>
2019-06-21 16:25:18 -0600

    It Works

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -5,7 +5,17 @@ const itunesService = new ItunesService()
 function drawSongs() {
   //changes button back to GET MUSIC once songs are loaded
   document.querySelector('#get-music-button').textContent = 'GET MUSIC'
-  console.log(itunesService.Songs)
+
+  // create a template variable
+  let songsElem = document.querySelector ('#songs')
+  let template = ''
+  let iTunesCopy = itunesService.Songs
+  iTunesCopy.forEach(song => {
+    template += song.Template
+  })
+  songsElem.innerHTML = template
+  // loop over the array of songs and add a template for each song to the template variable
+  // assign the template as the innerHTML of #songs
 
 }
 
@@ -14,6 +24,7 @@ function drawSongs() {
 class ItunesController {
   constructor() {
     //BE SURE TO REGISTER YOUR SUBSCRIBERS!!!!!!!
+    itunesService.addSubscriber('songs', drawSongs)    
   }
 
 
--- app/models/Song.js
+++ app/models/Song.js
@@ -2,10 +2,27 @@ export default class Song {
     constructor(song) {
         this.title = song.trackName
         //Change 250x250 if you want a larger image
-        this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+        this.albumArt = song.artworkUrl60.replace(/60x60/g, "200x200")
         this.artist = song.artistName
         this.collection = song.collectionName
         this.price = song.collectionPrice
         this.preview = song.previewUrl
+    }
+
+    get Template(){
+        return `
+        <div class="col-3 pt-2">
+            <div class="card" style="width: 18rem;">
+                <img src="${this.albumArt}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Artist: </b>${this.artist}</h5>
+                        <p class="card-text"><b>Title: </b>${this.title}</p>
+                        <p class="card-text"><b>Album: </b>${this.collection}</p>
+                        <p class="card-text"><b>Price: </b>${this.price}</p>
+                        <audio controls src="${this.preview}"></audio>
+                    </div>
+            </div>
+        </div>
+        `
     }
 }
